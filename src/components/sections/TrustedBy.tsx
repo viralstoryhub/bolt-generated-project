a7ba5d51d15d94703045a91ae054c328e0149a4a
viralstoryhub <viralstoryhub90@gmail.com>
2024-12-12 20:32:22 -0800

    Initial commit from your app

--- src/components/sections/TrustedBy.tsx
+++ src/components/sections/TrustedBy.tsx
@@ -2,52 +2,28 @@ import React from 'react';
 
 const companies = [
   {
-    name: 'Alibaba',
-    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Alibaba_logo.svg/1280px-Alibaba_logo.svg.png',
+    name: '10Web',
+    logo: 'https://www.webdew.com/wp-content/uploads/2023/12/10web-logo-1.png',
   },
   {
-    name: 'Arbitrum',
-    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Arbitrum_logo.svg/1280px-Arbitrum_logo.svg.png',
+    name: 'Microsoft',
+    logo: 'https://logos-world.net/wp-content/uploads/2020/09/Microsoft-Logo.png',
   },
   {
-    name: 'Cosmose AI',
-    logo: 'https://images.crunchbase.com/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/pfiwd775zmgib75l6f5c',
+    name: 'OpenAI',
+    logo: 'https://freepnglogo.com/images/all_img/1702059309openai-logo-png.png',
   },
   {
-    name: 'EigenLayer',
-    logo: 'https://eigenlayer.xyz/logo.png',
+    name: 'Anthropic AI',
+    logo: 'https://logovtor.com/wp-content/uploads/2021/06/anthropic-ai-logo-vector.png',
   },
   {
-    name: 'Google',
-    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png',
+    name: 'Nvidia',
+    logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdG1UQbKJH1swnJgLUBBUjs5HJoV6qNcsIDA&s',
   },
   {
-    name: 'ICC',
-    logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/5/52/International_Cricket_Council_logo.svg/1280px-International_Cricket_Council_logo.svg.png',
-  },
-  {
-    name: 'dropt',
-    logo: 'https://pbs.twimg.com/profile_images/1674824449836195840/5-Hbcdgq_400x400.jpg',
-  },
-  {
-    name: 'SAIL GP',
-    logo: 'https://sailgp.com/site-elements/images/sailgp-logo-white.svg',
-  },
-  {
-    name: 'Shemaroo',
-    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Shemaroo_new_logo.svg/2560px-Shemaroo_new_logo.svg.png',
-  },
-  {
-    name: 'MARBLEX',
-    logo: 'https://www.marblx.io/img/logo.png',
-  },
-  {
-    name: 'Polygon',
-    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Polygon_blockchain_logo.svg/1280px-Polygon_blockchain_logo.svg.png',
-  },
-  {
-    name: 'SWEAT',
-    logo: 'https://pbs.twimg.com/profile_images/1696864979375550464/IvAN-7-I_400x400.jpg',
+    name: 'AWS',
+    logo: 'https://download.logo.wine/logo/Amazon_Web_Services/Amazon_Web_Services-Logo.wine.png',
   },
 ];
 
